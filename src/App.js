9a66d871781381b7f48297a52e6d050288830679
gitraya <mail.raya15@gmail.com>
2021-03-20 23:08:15 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -8,11 +8,29 @@ const App = () => {
       trueAnswer: null,
       falseAnswer: [],
     },
+    option: [],
+    alfabet: ['A', 'B', 'C', 'D'],
+  });
+  const [answerStyles, setAnswerStyles] = useState({
+    true: { background: '#EA8282' },
   });
 
   const randomNum = function (max) {
-    let number = Math.floor(Math.random() * max) + 1;
+    let number = Math.floor(Math.random() * max);
     return number;
+  };
+
+  const shuffle = (arr) => {
+    for (let i = arr.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+
+      // swap elements array[i] and array[j]
+      // we use "destructuring assignment" syntax to achieve that
+      // you'll find more details about that syntax in later chapters
+      // same can be written as:
+      // let t = array[i]; array[i] = array[j]; array[j] = t
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
   };
 
   function generateUniqueRandom(max, arr) {
@@ -31,7 +49,8 @@ const App = () => {
   }
 
   const storeCapitalQuiz = () => {
-    const trueCountry = allCountries[randomNum(250)];
+    let trueCountry = allCountries[randomNum(250)];
+    if (!trueCountry.capital) trueCountry = allCountries[randomNum(250)];
     let falseCountry = [];
     let randArr = [];
 
@@ -40,16 +59,47 @@ const App = () => {
       falseCountry.push(allCountries[rand].name);
     }
 
+    const option = [trueCountry.name, ...falseCountry];
+    shuffle(option);
+
     const updateCapitalQuiz = {
       question: trueCountry.capital,
       answer: {
         trueAnswer: trueCountry.name,
         falseAnswer: falseCountry,
       },
+      option: option,
+      alfabet: capitalQuiz.alfabet,
     };
 
     setCapitalQuiz(updateCapitalQuiz);
   };
+
+  const checkAnswer = (answer) => {
+    if (answer === capitalQuiz.answer.trueAnswer) {
+      storeCapitalQuiz();
+      return console.log(true, 'Kamu benar');
+    }
+    return console.log(false, 'Kamu salah');
+  };
+
+  const answerButtonRender = capitalQuiz.option.map((answer, i) => {
+    return (
+      <button
+        key={i}
+        className="answer-button"
+        onClick={() => {
+          checkAnswer(answer);
+        }}
+      >
+        <div className="wrap-answer">
+          <span className="alfa-text">{capitalQuiz.alfabet[i]}</span>
+          <span className="answer-text">{answer}</span>
+        </div>
+        <i>i</i>
+      </button>
+    );
+  });
 
   const fetchingCountry = async () => {
     await fetch('https://restcountries.eu/rest/v2/all')
@@ -65,33 +115,46 @@ const App = () => {
   });
 
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${
+          process.env.PUBLIC_URL + '/images/background.png'
+        })`,
+      }}
+    >
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button
-          onClick={() => {
-            console.log(allCountries);
-            storeCapitalQuiz();
-          }}
-        >
-          Klik
-        </button>
-        <button
-          onClick={() => {
-            console.log(capitalQuiz);
-          }}
-        >
-          lihat
-        </button>
+        <button onClick={storeCapitalQuiz}>Mulai</button>
       </header>
-      <main></main>
+      <main className="App-main">
+        <div className="container">
+          <h1 className="app-title">country quiz</h1>
+          <img
+            className="image-onquiz"
+            src={process.env.PUBLIC_URL + '/images/undraw_adventure_4hum 1.svg'}
+            alt="hero"
+          />
+          <div className="quiz-card">
+            <h2 className="question-text">{`${capitalQuiz.question} is the capital of`}</h2>
+            {answerButtonRender}
+          </div>
+        </div>
+      </main>
+      <footer className="App-footer">
+        <small className="copyright">
+          {`created by `}
+          <b>
+            <a
+              href="https://github.com/gitraya"
+              target="_blank"
+              rel="noreferrer"
+            >
+              gitraya
+            </a>
+          </b>
+          {`- devChallenges.io`}
+        </small>
+      </footer>
     </div>
   );
 };
